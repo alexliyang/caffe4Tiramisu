a6338c2039e655bf5d073c633998490a7737fd97
Dario Garcia <dariog@lsi.upc.edu>
2015-11-05 09:36:49 +0100

    Added version for binary ivf. Half the space required.

--- tools/extract_featuresTiramisu.cpp
+++ tools/extract_featuresTiramisu.cpp
@@ -127,7 +127,10 @@ int feature_extraction_pipeline(int argc, char** argv) {
   std::vector<Blob<float>*> input_vec;
   std::ofstream file;
   LOG(ERROR)<< "Writing to file: " << dataset_names[0].c_str() << fileName<<".ivf";
+  //CHAR
   file.open((dataset_names[0]+fileName+".ivf").c_str(),std::ios::trunc);
+  //BINARY
+  //file.open((dataset_names[0]+fileName+".ivf").c_str(),std::ios::binary);
   std::ofstream file_cfg;
   LOG(ERROR)<< "Writing to file: " << dataset_names[0].c_str() << fileName<<".cfg";
   file_cfg.open((dataset_names[0]+fileName+".cfg").c_str(),std::ios::trunc);
@@ -147,7 +150,15 @@ int feature_extraction_pipeline(int argc, char** argv) {
 
     feature_blob_data = feature_blob->cpu_data() + feature_blob->offset(0);
     for (int d = 0; d < dim_features; ++d) {
+      //CHAR
       if(feature_blob_data[d] > 0) file<<feature_blob_data[d]<<" "<<d+offset<<"\n";
+      //BINARY
+      //if(feature_blob_data[d] > 0) {
+      //  int position = d + offset;
+      //  float value = feature_blob_data[d]; 
+      //  file.write(reinterpret_cast<char *>(&position), sizeof(position));
+      //  file.write(reinterpret_cast<char *>(&value), sizeof(value));
+      //}
     } 
     offset+=dim_features;   
   }  
